a32b89e946e93c99f3130127e0841c2a75806c1c
Leekiseong02 <leekisung0212@gmail.com>
2021-08-26 03:10:44 +0900

    push

--- lib/firebase/API.dart
+++ lib/firebase/API.dart
@@ -1,18 +1,18 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
-class AuthEmailAccount{
-  AuthEmailAccount({required this.email,required this.password});
-  String email;
-  String password;
+// class AuthEmailAccount{
+//   AuthEmailAccount({required this.email,required this.password});
+//   String email;
+//   String password;
 
-  void signUP(String email, String password) async{
-    FirebaseAuth auth = FirebaseAuth.instance;
-    try {
-      final newUser = await auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-    } catch (e) {
-      print(e);
-    }
-  }
-}
+//   void signUP(String email, String password) async{
+//     FirebaseAuth auth = FirebaseAuth.instance;
+//     try {
+//       final newUser = await auth.createUserWithEmailAndPassword(
+//           email: email, password: password);
+//     } catch (e) {
+//       print(e);
+//     }
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:i_am_exercising/main_screen.dart';
 import 'package:i_am_exercising/planning_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'I AM WORKING OUT',
       theme: ThemeData(
