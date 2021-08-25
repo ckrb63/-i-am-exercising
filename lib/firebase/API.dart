5e1b6a816577473dfbc1f29e28117dd84a7c9dad
ckrb63 <cksr1@naver.com>
2021-08-26 01:41:12 +0900

    api

---
+++ lib/firebase/API.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthEmailAccount{
+  AuthEmailAccount({required this.email,required this.password});
+  String email;
+  String password;
+
+  void signUP(String email, String password) async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    try {
+      final newUser = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'firebase/API.dart';
 
 class MainScreen extends StatelessWidget {
   @override
